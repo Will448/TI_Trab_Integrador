11b68e78e3856b4aa85bdf65bd22cde9d0cc8973
Willian <92097335+Will448@users.noreply.github.com>
2022-06-29 11:28:02 -0300

    Update tela_delete.php

--- TI-CEMPE/screens/login/tela_delete.php
+++ TI-CEMPE/screens/login/tela_delete.php
@@ -1,6 +1,19 @@
-
+<?php
+// Inicialize a sessão
+session_start();
+ 
+// Verifique se o usuário já está logado, em caso afirmativo, redirecione-o para a página de boas-vindas
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: tela_inicial.php");
+    exit;
+}
+ 
+// Incluir arquivo de configuração
+require_once "../database/bd_login.php";
 
 $sql = "DELETE FROM user WHERE id =  :id";
 $stmt = $pdo->prepare($sql);
 $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_INT);   
 $stmt->execute();
+
+?>
